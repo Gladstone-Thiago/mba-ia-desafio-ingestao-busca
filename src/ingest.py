aecb1bdde2d718679d06b8c4bf45ff9bee027b18
thiago <tgladstone@coamo.com.br>
2025-09-08 22:14:21 -0300

    feat(ingest) criado load e insert no pgvector

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,46 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
 
 def ingest_pdf():
-    pass
+    docs = PyPDFLoader(str(PDF_PATH)).load()
+    
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=150, add_start_index=False).split_documents(docs)
+    
+    enriched = [
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
+    ]
+    ids = [f"doc-{i}" for i in range(len(enriched))]
 
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model=os.getenv("GOOGLE_EMBEDDING_MODEL", "models/embedding-001"),
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+    )
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
 
 if __name__ == "__main__":
     ingest_pdf()
